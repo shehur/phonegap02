d0ea5e7baa8a44711547e80f73cc198d66403099
shehur <shehur5727@gmail.com>
2018-10-27 13:20:30 -0300

    27_10_2018-13_20

---
+++ www/js/index.js
@@ -0,0 +1,25 @@
+﻿var app = {
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    receivedEvent: function(id) {
+        var nivel = document.getElementById('nivel');
+
+        window.addEventListener("batterystatus", onBatteryStatus, false);
+
+        function onBatteryStatus(status) {
+            if(status.isPlugged) {
+                nivel.innerText = "Nível: " + status.level + "% - Carregando: SIM";
+            }else {
+                nivel.innerText = "Nível: " + status.level + "% - Carregando: NÃO";
+            }
+        }
+    }
+};
+
+app.initialize();
